3933bb04321307acc29b57203202a6fa7d7e99bf
Francisco de Assis Boldt <fboldt@gmail.com>
2021-11-25 11:57:36 -0300

    improving legibility

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const servidor = require('./modules/servidor');
+const hello = require('./modules/hello');
+const echopayload = require('./modules/echopayload');
 
 const app = servidor.novoServidor();
 
-app.addRota('hello', require('./modules/hello').handler);
-app.addRota('echopayload', require('./modules/echopayload').handler);
+app.addRota('hello', hello.handler);
+app.addRota('echopayload', echopayload.handler);
 
 const porta = process.env.PORT || 3000;
 app.ativar(porta);
